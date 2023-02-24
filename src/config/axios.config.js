5c2b9d39b239276e5b0e9ce305adbd22e14e1223
tananine <tana9dev@gmail.com>
2023-02-24 15:52:21 +0700

    change baseURL axios to local

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,7 +1,7 @@
 import Cookies from 'js-cookie';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://192.168.1.17:8080';
+axios.defaults.baseURL = 'http://localhost:8080';
 
 const authToken = Cookies.get('authToken');
 if (authToken) {
