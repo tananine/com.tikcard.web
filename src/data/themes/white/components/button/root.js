d4bd0388c523f70f1c697c57d961e686cd704487
tananine <tana9dev@gmail.com>
2023-02-26 19:07:06 +0700

    update theme button default

--- src/data/themes/white/components/button/root.js
+++ src/data/themes/white/components/button/root.js
@@ -1,9 +1,16 @@
+import typography from 'data/themes/white/base/typography';
 import colors from 'data/themes/white/base/colors';
+import pxToRem from 'data/themes/white/functions/pxToRem';
 
-const { black } = colors;
+const { black, white } = colors;
+
+const { fontWeightBold } = typography;
 
 const root = {
   color: black.main,
+  minHeight: pxToRem(40.5),
+  fontWeight: fontWeightBold,
+  backgroundColor: white,
 };
 
 export default root;
