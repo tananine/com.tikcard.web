82c789f6b72807462e394f4a86b3229f28e02bfd
tananine <tana9dev@gmail.com>
2023-03-04 08:20:47 +0700

    add api get list my contact in profile body page

--- src/pages/profile/body/index.js
+++ src/pages/profile/body/index.js
@@ -1,35 +1,47 @@
-import { useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { DndContext } from '@dnd-kit/core';
-import { Divider, Typography } from '@mui/material';
+import { Box, CircularProgress, Divider, Typography } from '@mui/material';
 import { SortableContext } from '@dnd-kit/sortable';
 import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
 import ContactList from 'pages/profile/body/ContactList';
+import { useSelector } from 'react-redux';
 
-const items = [
-  {
-    id: 1,
-  },
-  {
-    id: 2,
-  },
-  {
-    id: 3,
-  },
-  {
-    id: 4,
-  },
-  {
-    id: 5,
-  },
-  {
-    id: 6,
-  },
-];
+import useGet from 'hooks/axios/useGet';
+import profileService from 'data/jsons/services/profile.service.json';
 
 const ProfileBody = () => {
   const handleDragEnd = useCallback(() => {}, []);
 
   const handleDragOver = useCallback(() => {}, []);
+
+  const [items, setItems] = useState([]);
+
+  const [getMyContactAction, getMyContactLoading, getMyContactData] = useGet(
+    profileService.getMyContact
+  );
+
+  const profileId = useSelector(
+    (state) => state.controller.profileInUse.profileId
+  );
+
+  useEffect(() => {
+    getMyContactAction(profileId).then((res) => {
+      setItems(res.data);
+    });
+  }, [getMyContactAction, profileId]);
+
+  if (getMyContactLoading || !getMyContactData) {
+    return (
+      <Box
+        position="absolute"
+        top="50%"
+        left="50%"
+        sx={{ transform: 'translate(-50%, -50%)' }}
+      >
+        <CircularProgress disableShrink />
+      </Box>
+    );
+  }
 
   return (
     <>
@@ -42,7 +54,7 @@ const ProfileBody = () => {
       >
         <SortableContext items={items}>
           {items.map((item) => (
-            <ContactList key={item.id} id={item.id} />
+            <ContactList key={item.id} id={item.id} name={item.ContactItem.name} data={item.urlUnique} />
           ))}
         </SortableContext>
       </DndContext>
