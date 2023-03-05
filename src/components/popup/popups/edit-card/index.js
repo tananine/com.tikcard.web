de06a4d5da00ca841f73ee6da73e9370d1062033
tananine <tana9dev@gmail.com>
2023-03-05 23:35:14 +0700

    add form in edit card

--- src/components/popup/popups/edit-card/index.js
+++ src/components/popup/popups/edit-card/index.js
@@ -3,12 +3,16 @@ import PopupWrapper from 'components/popup/PopupWrapper';
 import { useSelector, useDispatch } from 'react-redux';
 import { editCardToggle } from 'stores/popup';
 import { Box, Button, TextField } from '@mui/material';
+import { useForm } from 'react-hook-form';
+
 import ProfileImageHead from 'components/popup/popups/edit-card/ProfileImageHead';
 
 const EditCard = () => {
   const open = useSelector((state) => state.popup.editCardPopup);
 
   const dispatch = useDispatch();
+
+  const { register, handleSubmit, setValue } = useForm();
 
   const editCardToggleHandler = useCallback(() => {
     dispatch(editCardToggle());
@@ -21,18 +25,56 @@ const EditCard = () => {
       onOpen={editCardToggleHandler}
     >
       <Box height="100vh" paddingY={1}>
-        <TextField label="ชื่อนามบัตร" variant="outlined" fullWidth />
+        <TextField
+          label="ชื่อนามบัตร"
+          variant="outlined"
+          fullWidth
+          InputLabelProps={{ shrink: true }}
+          {...register('profileName')}
+        />
         <ProfileImageHead />
         <Box marginTop={4} display="flex" flexDirection="column" gap={2}>
-          <TextField label="ชื่อ" variant="outlined" fullWidth />
-          <TextField label="อาชีพ" variant="outlined" fullWidth />
-          <TextField label="บริษัท" variant="outlined" fullWidth />
-          <TextField label="ตำแหน่ง" variant="outlined" fullWidth />
-          <TextField label="ที่อยู่" variant="outlined" fullWidth />
+          <TextField
+            label="ชื่อ"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            {...register('name')}
+          />
+          <TextField
+            label="อาชีพ"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            {...register('work')}
+          />
+          <TextField
+            label="บริษัท"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            {...register('company')}
+          />
+          <TextField
+            label="ตำแหน่ง"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            {...register('position')}
+          />
+          <TextField
+            label="ที่อยู่"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            {...register('address')}
+          />
           <TextField
             label="เกี่ยวกับฉัน"
             variant="outlined"
             fullWidth
+            InputLabelProps={{ shrink: true }}
+            {...register('bio')}
             multiline
             rows={3}
           />
