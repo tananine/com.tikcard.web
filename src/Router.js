e53bc48da2e3f31ca67e66e6e97e002f1c021521
tananine <tana9dev@gmail.com>
2023-03-22 23:36:26 +0700

    add ViewIndex page

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 
 import App from 'App';
+import ViewIndex from 'ViewIndex';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/app/:page" element={<App />} />
+      <Route path="/:linkId" element={<ViewIndex />} />
     </Routes>
   );
 };
