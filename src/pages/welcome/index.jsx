781f8e9a345cf543941b90f94155b3250ef19292
tananine <tana9dev@gmail.com>
2023-03-30 21:22:11 +0700

    add flicking in welcome page

--- src/pages/welcome/index.jsx
+++ src/pages/welcome/index.jsx
@@ -1,9 +1,15 @@
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
+import '@egjs/react-flicking/dist/flicking.css';
+import Flicking from '@egjs/react-flicking';
 
 const Welcome = () => {
   return (
     <Box height="100vh">
-      <Typography>Welcome</Typography>
+      <Flicking align="prev" circular={true}>
+        <Box width="100vw">สร้างนามบัตรแรกกันเลย</Box>
+        <Box width="100vw">นามบัตรนี้ใช้สำหรับ</Box>
+        <Box width="100vw">ใส่ข้อมูลซักหน่อย (ข้าม)</Box>
+      </Flicking>
     </Box>
   );
 };
