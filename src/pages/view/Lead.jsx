23a7b2906144fa32524d681ed7bc13529b612194
tananine <tana9dev@gmail.com>
2023-04-13 00:05:00 +0700

    add Lead component

---
+++ src/pages/view/Lead.jsx
@@ -0,0 +1,88 @@
+import {
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  TextField,
+  DialogActions,
+  Button,
+} from '@mui/material';
+import { useEffect, useState, useRef } from 'react';
+
+const Lead = () => {
+  const [openForm, setOpenForm] = useState(false);
+  const timeoutRef = useRef(null);
+
+  const openFormHandler = () => {
+    setOpenForm(true);
+    clearTimeout(timeoutRef.current);
+  };
+  const closeFormHandler = () => {
+    setOpenForm(false);
+  };
+
+  useEffect(() => {
+    timeoutRef.current = setTimeout(() => {
+      setOpenForm(true);
+    }, 3500);
+
+    return () => {
+      clearTimeout(timeoutRef.current);
+    };
+  }, []);
+
+  return (
+    <>
+      <Button
+        variant="contained"
+        fullWidth
+        size="large"
+        onClick={openFormHandler}
+      >
+        ฝากข้อมูลติดต่อ
+      </Button>
+      <Dialog
+        open={openForm}
+        maxWidth="xl"
+        PaperProps={{ sx: { margin: 2, width: '100%', borderRadius: 6 } }}
+      >
+        <DialogTitle>ข้อมูลติดต่อกลับของคุณ</DialogTitle>
+        <DialogContent>
+          <TextField
+            label="ชื่อ"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            sx={{ marginTop: 1 }}
+          />
+          <TextField
+            label="เบอร์โทร"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            sx={{ marginTop: 2 }}
+          />
+          <TextField
+            label="อีเมล"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            sx={{ marginTop: 2 }}
+          />
+          <TextField
+            label="ข้อความ"
+            variant="outlined"
+            fullWidth
+            InputLabelProps={{ shrink: true }}
+            sx={{ marginTop: 2 }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={closeFormHandler}>ปิด</Button>
+          <Button>ส่ง</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+};
+
+export default Lead;
--- src/pages/view/index.jsx
+++ src/pages/view/index.jsx
@@ -14,6 +14,8 @@ import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import openAppUri from '@/functions/openAppUri';
 import saveVCard from '@/functions/saveVCard';
 
+import Lead from '@/pages/view/Lead';
+
 const ContactLists = (contacts) => {
   return contacts?.map((contact, index) => {
     return (
@@ -55,97 +57,97 @@ const View = ({ isPreview, profileData }) => {
   };
 
   return (
-    <Box>
-      <Box
-        height={180}
-        bgcolor="#ced4da"
-        sx={{ borderBottomRightRadius: 60 }}
-      />
-      <Grow
-        in={true}
-        sx={{
-          position: 'absolute',
-          left: 20,
-          top: isPreview ? 135 : 75,
-        }}
-        timeout={{ enter: 1000 }}
-      >
-        <Box>
-          <Avatar
-            sx={{
-              width: 140,
-              height: 140,
-              border: '4px solid white',
-            }}
-          />
-          <Avatar
-            sx={{
-              position: 'absolute',
-              left: 110,
-              top: 80,
-              width: 60,
-              height: 60,
-              border: '4px solid white',
-            }}
-          />
-        </Box>
-      </Grow>
-      <Box padding={2} minHeight="100vh">
-        <Fade in={true} timeout={{ enter: 1000 }}>
-          <Box>
-            <Box marginBottom={2}>
-              <Typography marginTop={5} textAlign="center" variant="h1">
-                {profileData?.info.name || 'ไม่มีชื่อ'}
-              </Typography>
-              <Typography marginTop={1} textAlign="center" variant="body2">
-                {profileData?.info.company}
-              </Typography>
-              <Typography marginTop={2} textAlign="center" variant="body2">
-                {profileData?.info.bio}
-              </Typography>
-            </Box>
-            <Grid container spacing={1} sx={{ marginY: 2 }}>
-              <Grid item xs={7}>
-                <Button variant="contained" fullWidth size="large">
-                  ฝากข้อมูลติดต่อ
-                </Button>
-              </Grid>
-              <Grid item xs={5}>
-                <Button
-                  variant="contained"
-                  fullWidth
-                  size="large"
-                  color="secondary"
-                  startIcon={<PersonAddIcon />}
-                  onClick={() => saveVCard(profileData?.info)}
-                >
-                  บันทึก
-                </Button>
-              </Grid>
-            </Grid>
-            <Divider variant="middle" />
-          </Box>
-        </Fade>
-        <Grid
-          container
-          rowSpacing={1}
-          columnSpacing={3}
-          marginTop={2}
-          paddingX={2}
+    <>
+      <Box>
+        <Box
+          height={180}
+          bgcolor="#ced4da"
+          sx={{ borderBottomRightRadius: 60 }}
+        />
+        <Grow
+          in={true}
+          sx={{
+            position: 'absolute',
+            left: 20,
+            top: isPreview ? 135 : 75,
+          }}
+          timeout={{ enter: 1000 }}
         >
-          {ContactLists(profileData?.contacts)}
-        </Grid>
-      </Box>
-      {!isPreview && (
-        <Box textAlign="center" padding={2}>
-          <Typography variant="caption">สร้างโดย</Typography>
-          <Typography variant="caption">Tikcard.me</Typography>
-          <Button variant="text" size="large" onClick={goNewCreate}>
-            สร้างนามบัตรของคุณ ฟรี
-          </Button>
+          <Box>
+            <Avatar
+              sx={{
+                width: 140,
+                height: 140,
+                border: '4px solid white',
+              }}
+            />
+            <Avatar
+              sx={{
+                position: 'absolute',
+                left: 110,
+                top: 80,
+                width: 60,
+                height: 60,
+                border: '4px solid white',
+              }}
+            />
+          </Box>
+        </Grow>
+        <Box padding={2} minHeight="100vh">
+          <Fade in={true} timeout={{ enter: 1000 }}>
+            <Box>
+              <Box marginBottom={2}>
+                <Typography marginTop={5} textAlign="center" variant="h1">
+                  {profileData?.info.name || 'ไม่มีชื่อ'}
+                </Typography>
+                <Typography marginTop={1} textAlign="center" variant="body2">
+                  {profileData?.info.company}
+                </Typography>
+                <Typography marginTop={2} textAlign="center" variant="body2">
+                  {profileData?.info.bio}
+                </Typography>
+              </Box>
+              <Grid container spacing={1} sx={{ marginY: 2 }}>
+                <Grid item xs={7}>
+                  <Lead />
+                </Grid>
+                <Grid item xs={5}>
+                  <Button
+                    variant="contained"
+                    fullWidth
+                    size="large"
+                    color="secondary"
+                    startIcon={<PersonAddIcon />}
+                    onClick={() => saveVCard(profileData?.info)}
+                  >
+                    บันทึก
+                  </Button>
+                </Grid>
+              </Grid>
+              <Divider variant="middle" />
+            </Box>
+          </Fade>
+          <Grid
+            container
+            rowSpacing={1}
+            columnSpacing={3}
+            marginTop={2}
+            paddingX={2}
+          >
+            {ContactLists(profileData?.contacts)}
+          </Grid>
         </Box>
-      )}
-    </Box>
+        {!isPreview && (
+          <Box textAlign="center" padding={2}>
+            <Typography variant="caption">สร้างโดย</Typography>
+            <Typography variant="caption">Tikcard.me</Typography>
+            <Button variant="text" size="large" onClick={goNewCreate}>
+              สร้างนามบัตรของคุณ ฟรี
+            </Button>
+          </Box>
+        )}
+      </Box>
+    </>
   );
 };
 
