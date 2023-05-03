338579989e992422af70c3738141a7053404b69c
tananine <tana9dev@gmail.com>
2023-05-04 00:48:45 +0700

    refactor axios.config.js show toast

--- src/config/axios.config.js
+++ src/config/axios.config.js
@@ -1,5 +1,6 @@
 import Cookies from 'js-cookie';
 import axios from 'axios';
+import toast from 'react-hot-toast';
 
 axios.defaults.baseURL = 'http://localhost:8080';
 
@@ -11,14 +12,20 @@ if (authToken) {
 axios.interceptors.response.use(
   (response) => response,
   (error) => {
-    const status = error?.response?.status;
+    const status = error.response.status;
+
     if (status === 401) {
       window.location.replace('/app/login');
     } else {
       console.error(
-        error?.response?.data || 'เกิดข้อผิดพลาดที่ไม่มี response data'
+        error.response.data || 'เกิดข้อผิดพลาดที่ไม่มี response data'
       );
     }
+
+    if (error.response.data.showClientMessage && error.response.data.message) {
+      toast.error(error.response.data.message);
+    }
+
     return Promise.reject(error);
   }
 );
