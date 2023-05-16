ba763388f0e93836e46964f1a7778ced84075e74
tananine <tana9dev@gmail.com>
2023-05-16 23:31:24 +0700

    set default coverImage

--- src/functions/splitCoverImage.js
+++ src/functions/splitCoverImage.js
@@ -1,28 +1,26 @@
 import coverImage from '@/data/jsons/cover-image.json';
 
 const splitCoverImage = (data, typeReturn) => {
-  if (data) {
-    const arr = data.split('$');
-    const type = arr[0];
-    const coverId = arr[1];
-    const colorId = arr[2];
+  const arr = data ? data.split('$') : ['json', 1, 1];
+  const type = arr[0];
+  const coverId = arr[1];
+  const colorId = arr[2];
 
-    if (typeReturn === 'url') {
-      const objectImageColor = coverImage.colors.find(
-        (color) => color.id === parseInt(colorId)
-      ).objectImage;
-      const coverImageURL = coverImage.data.find(
-        (coverItem) => coverItem.id === parseInt(coverId)
-      ).coverImage[objectImageColor];
-      return coverImageURL;
-    }
-
-    return {
-      type: type,
-      coverId: coverId,
-      colorId: colorId,
-    };
+  if (typeReturn === 'url') {
+    const objectImageColor = coverImage.colors.find(
+      (color) => color.id === parseInt(colorId)
+    ).objectImage;
+    const coverImageURL = coverImage.data.find(
+      (coverItem) => coverItem.id === parseInt(coverId)
+    ).coverImage[objectImageColor];
+    return coverImageURL;
   }
+
+  return {
+    type: type,
+    coverId: coverId,
+    colorId: colorId,
+  };
 };
 
 export default splitCoverImage;
