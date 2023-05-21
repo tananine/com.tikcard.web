c167b49cec28b38a7fb30d2f8932415c3984af62
tananine <tana9dev@gmail.com>
2023-05-21 22:34:05 +0700

    change header title

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useCallback } from 'react';
-import { Box, Container, IconButton } from '@mui/material';
-import { useDispatch } from 'react-redux';
+import { Box, Container, IconButton, Typography } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
 import { setHeaderHeight } from '@/stores/offset';
 import { menuToggle } from '@/stores/drawer';
 import { useParams } from 'react-router-dom';
@@ -10,11 +10,29 @@ import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 import Logo from '@/assets/images/logo.png';
 
+const TitleProfileState = () => {
+  const profileState = useSelector(
+    (state) => state.controller.profileInUse.profileState
+  );
+
+  if (profileState === 'primary') {
+    return <Typography variant="h3">นามบัตร 1</Typography>;
+  }
+
+  if (profileState === 'secondary') {
+    return <Typography variant="h3">นามบัตร 2</Typography>;
+  }
+
+  return <Box />;
+};
+
 const Header = ({ component }) => {
   const dispatch = useDispatch();
   const headerRef = useRef();
 
   const { page } = useParams();
+
+  const isScanDouble = useSelector((state) => state.device.isScanDouble);
 
   const setHeaderHeightHandler = useCallback(() => {
     if (headerRef.current) {
@@ -34,7 +52,11 @@ const Header = ({ component }) => {
     <Container ref={headerRef}>
       <Box paddingX={2} paddingTop={1} paddingBottom={1}>
         <Box display="flex" justifyContent="space-between" alignItems="center">
-          <Img src={Logo} alt="" height="22px" />
+          {isScanDouble ? (
+            <TitleProfileState />
+          ) : (
+            <Img src={Logo} alt="" height="22px" />
+          )}
           <IconButton onClick={menuToggleHandler}>
             <MoreHorizIcon sx={{ fontSize: '30px' }} />
           </IconButton>
