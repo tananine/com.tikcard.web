b9e30539aab2802411283a53b7401891492f0eab
tananine <tana9dev@gmail.com>
2023-08-14 12:59:55 +0700

    re design login open uri

--- src/functions/openAppUri.js
+++ src/functions/openAppUri.js
@@ -2,15 +2,20 @@ const openAppUri = (defaultUri, androidUri, iosUri, data) => {
   const userAgent = navigator.userAgent;
 
   if (/android/i.test(userAgent)) {
+    if (androidUri !== '$') {
+      return window.open(androidUri.replace('$', data), '_self');
+    }
     const uri =
-      iosUri.replace('$', data).includes('https://') ||
-      iosUri.replace('$', data).includes('http://') ||
-      iosUri !== '$'
-        ? window.open(iosUri.replace('$', data), '_blank')
-        : window.open('https://' + iosUri.replace('$', data), '_blank');
+      androidUri.replace('$', data).includes('https://') ||
+      androidUri.replace('$', data).includes('http://')
+        ? window.open(androidUri.replace('$', data), '_blank')
+        : window.open('https://' + androidUri.replace('$', data), '_blank');
 
     return uri;
   } else if (/iPad|iPhone|iPod/.test(userAgent)) {
+    if (iosUri !== '$') {
+      return window.open(iosUri.replace('$', data), '_self');
+    }
     const uri =
       iosUri.replace('$', data).includes('https://') ||
       iosUri.replace('$', data).includes('http://') ||
@@ -21,6 +26,9 @@ const openAppUri = (defaultUri, androidUri, iosUri, data) => {
     return uri;
   }
 
+  if (defaultUri !== '$') {
+    return window.open(defaultUri.replace('$', data), '_self');
+  }
   const uri =
     defaultUri.replace('$', data).includes('https://') ||
     defaultUri.replace('$', data).includes('http://') ||
