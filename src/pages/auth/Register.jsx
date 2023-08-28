a96340ed046c01b353129ec8052793cf0f72d1c0
tananine <tana9dev@gmail.com>
2023-08-28 08:25:23 +0700

    add policy

--- src/pages/auth/Register.jsx
+++ src/pages/auth/Register.jsx
@@ -2,7 +2,9 @@ import { LoadingButton } from '@mui/lab';
 import {
   Box,
   Button,
+  Checkbox,
   Container,
+  FormControlLabel,
   Paper,
   Skeleton,
   TextField,
@@ -15,11 +17,14 @@ import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import toast from 'react-hot-toast';
 
+import Policy from '@/components/Policy';
+
 import authServicePath from '@/data/jsons/services/auth.service.json';
 
 import usePost from '@/hooks/axios/usePost';
 
 import Logo from '@/assets/images/logo.png';
+import { useState } from 'react';
 
 const schema = yup
   .object({
@@ -43,6 +48,22 @@ const schema = yup
 const Register = () => {
   const navigate = useNavigate();
 
+  const [checkPolicy, setCheckPolicy] = useState(false);
+  const [openDialogPolicy, setOpenDialogPolicy] = useState(false);
+
+  const openPolicyHandler = (e) => {
+    e.preventDefault();
+    setOpenDialogPolicy(true);
+  };
+
+  const closePolicyHandler = () => {
+    setOpenDialogPolicy(false);
+  };
+
+  const checkPolicyHandler = (e) => {
+    setCheckPolicy(e.target.checked);
+  };
+
   const currentUrl = window.location.href;
   const query =
     currentUrl.split('?').length >= 2 ? currentUrl.split('?')[1] : '';
@@ -61,6 +82,12 @@ const Register = () => {
   });
 
   const registerFunction = async (form) => {
+    if (!checkPolicy) {
+      return alert(
+        'กรุณายอมรับเงื่อนไขในการใช้งานเว็บไซต์ และ นโยบายความเป็นส่วนตัว'
+      );
+    }
+
     const body = {
       email: form.email,
       password: form.password,
@@ -74,97 +101,118 @@ const Register = () => {
   };
 
   return (
-    <Container>
-      <Paper
-        elevation={3}
-        sx={{
-          height: '100svh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-        }}
-      >
-        <Box
-          component="form"
-          paddingX={2}
-          paddingTop={6}
-          display="grid"
-          gap={2}
-          overflow="auto"
-          onSubmit={handleSubmit(registerFunction)}
+    <>
+      <Container>
+        <Paper
+          elevation={3}
+          sx={{
+            height: '100svh',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+          }}
         >
-          <Box textAlign="center">
-            <Img
-              src={Logo}
-              alt=""
-              height="32px"
-              loader={
-                <Skeleton
-                  animation="wave"
-                  variant="rounded"
-                  width="120px"
-                  height="32px"
-                  sx={{ margin: 'auto' }}
-                />
+          <Box
+            component="form"
+            paddingX={2}
+            paddingTop={6}
+            display="grid"
+            gap={2}
+            overflow="auto"
+            onSubmit={handleSubmit(registerFunction)}
+          >
+            <Box textAlign="center">
+              <Img
+                src={Logo}
+                alt=""
+                height="32px"
+                loader={
+                  <Skeleton
+                    animation="wave"
+                    variant="rounded"
+                    width="120px"
+                    height="32px"
+                    sx={{ margin: 'auto' }}
+                  />
+                }
+              />
+            </Box>
+            <Typography textAlign="center" variant="h5">
+              สร้างบัญชีผู้ใช้
+            </Typography>
+            <Box display="grid" gap={2}>
+              <TextField
+                label="อีเมล"
+                variant="outlined"
+                fullWidth
+                type="email"
+                error={!!errors?.email}
+                helperText={errors?.email?.message}
+                {...register('email')}
+              />
+              <TextField
+                label="รหัสผ่าน"
+                variant="outlined"
+                fullWidth
+                type="password"
+                error={!!errors?.password}
+                helperText={errors?.password?.message}
+                {...register('password')}
+              />
+              <TextField
+                label="ยืนยันรหัสผ่าน"
+                variant="outlined"
+                fullWidth
+                type="password"
+                error={!!errors?.confirmPassword}
+                helperText={errors?.confirmPassword?.message}
+                {...register('confirmPassword')}
+              />
+            </Box>
+            <FormControlLabel
+              control={
+                <Checkbox checked={checkPolicy} onChange={checkPolicyHandler} />
+              }
+              label={
+                <Typography variant="body2">
+                  ฉันยอมรับเงื่อนไข{' '}
+                  <Typography
+                    component="span"
+                    variant="body2"
+                    onClick={openPolicyHandler}
+                    sx={{ textDecoration: 'underline' }}
+                  >
+                    นโยบายความเป็นส่วนตัว
+                  </Typography>
+                </Typography>
               }
             />
+            <Box display="grid" gap={2} marginBottom={12}>
+              <LoadingButton
+                variant="contained"
+                color="secondary"
+                size="large"
+                fullWidth
+                type="submit"
+                loading={registerLoading}
+              >
+                สร้างบัญชี
+              </LoadingButton>
+              <Button
+                onClick={() =>
+                  navigate('/app/login' + (query && '?' + query), {
+                    replace: true,
+                  })
+                }
+              >
+                กลับ
+              </Button>
+            </Box>
           </Box>
-          <Typography textAlign="center" variant="h5">
-            สร้างบัญชีผู้ใช้
-          </Typography>
-          <Box display="grid" gap={2}>
-            <TextField
-              label="อีเมล"
-              variant="outlined"
-              fullWidth
-              type="email"
-              error={!!errors?.email}
-              helperText={errors?.email?.message}
-              {...register('email')}
-            />
-            <TextField
-              label="รหัสผ่าน"
-              variant="outlined"
-              fullWidth
-              type="password"
-              error={!!errors?.password}
-              helperText={errors?.password?.message}
-              {...register('password')}
-            />
-            <TextField
-              label="ยืนยันรหัสผ่าน"
-              variant="outlined"
-              fullWidth
-              type="password"
-              error={!!errors?.confirmPassword}
-              helperText={errors?.confirmPassword?.message}
-              {...register('confirmPassword')}
-            />
-          </Box>
-          <Box display="grid" gap={2} marginBottom={12}>
-            <LoadingButton
-              variant="contained"
-              color="secondary"
-              size="large"
-              fullWidth
-              type="submit"
-              loading={registerLoading}
-            >
-              สร้างบัญชี
-            </LoadingButton>
-            <Button
-              onClick={() =>
-                navigate('/app/login' + (query && '?' + query), {
-                  replace: true,
-                })
-              }
-            >
-              กลับ
-            </Button>
-          </Box>
-        </Box>
-      </Paper>
-    </Container>
+        </Paper>
+      </Container>
+      <Policy open={openDialogPolicy} onClose={closePolicyHandler} />
+    </>
   );
 };
 
